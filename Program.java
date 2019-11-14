3f06f9d57bf69a0bd27259d0ee9eb8b719fc385c
MichelELL <michel.mottet@hotmail.com>
2019-11-14 12:18:51 +0100

    do builder tree

--- Program.java
+++ Program.java
@@ -18,7 +18,7 @@ public class Program {
 				if (args.length < 0)
 					throw new Exception("Incorrect number of parameters to program.");
 
-				inputFileName = "C:\\Users\\fajo6716\\IdeaProjects\\Prop-A0\\.idea\\program2.txt";
+				inputFileName = "C:\\Users\\Michel\\IdeaProjects\\Prop-A0\\program2.txt";
 				//outputFileName = "C:\\Users\\Michel\\IdeaProjects\\Prop-A0\\out.txt";
 
 				// inputFileName = args[0];
