f6b36b375f281ebe00a50527a31fe0c2906471c8
MichelELL <michel.mottet@hotmail.com>
2019-11-15 09:20:20 +0100

    StringBuilder Almost Complete

--- Parser.java
+++ Parser.java
@@ -56,9 +56,9 @@ public class Parser implements IParser {
             System.out.println("BlockNode ");
             if (tok.current().token() == Token.LEFT_CURLY) {
                 System.out.println(tok.current());
-                lex = tok.current();
+               // lex = tok.current();
                 //outBuilder.append(lex.toString());
-                buildString(outBuilder,0);
+             //   buildString(outBuilder,0);
                 tok.moveNext();
                 s = new StatementsNode(tok);
                 System.out.println("are wer here?");
@@ -66,7 +66,7 @@ public class Parser implements IParser {
                 if (tok.current().token() == Token.RIGHT_CURLY) {
                     System.out.println(tok.current());
                     lex = tok.current();
-                    buildString(outBuilder,0);
+                  //  buildString(outBuilder,0);
                     tok.moveNext();
                    // outBuilder.append(lex.toString());
                     if (tok.current().token() != Token.EOF) {
@@ -89,8 +89,10 @@ public class Parser implements IParser {
         @Override
         public void buildString(StringBuilder builder, int tabs) {
             builder.append("BlockNode" + "\r\n");
-            builder.append(lex + "\r\n");
-            builder.append("TEST TEST TEST TEST " + "\r\n" + lex);
+            builder.append(Token.LEFT_CURLY + "\r\n");
+            tabs++;
+            s.buildString(builder, tabs);
+           // builder.append("TEST TEST TEST TEST " + "\r\n" + lex);
 
             //recursive genom alla
         }
@@ -104,10 +106,12 @@ public class Parser implements IParser {
 
         private AssignmentNode aN = null;
         private StatementsNode sN = null;
+        private Lexeme lex = null;
 
         public StatementsNode(Tokenizer tok) throws TokenizerException, IOException {
             System.out.println("StatementNode ");
             if (tok.current().token() == Token.IDENT) {
+                lex = tok.current();
                 aN = new AssignmentNode(tok);
                 sN = new StatementsNode(tok);
             }
@@ -121,7 +125,12 @@ public class Parser implements IParser {
 
         @Override
         public void buildString(StringBuilder builder, int tabs) {
-
+            builder.append("StatementNode" + "\r\n");
+            tabs++;
+            if (lex != null) {
+                aN.buildString(builder, tabs);
+                sN.buildString(builder, tabs);
+            }
         }
     }
 
@@ -135,11 +144,12 @@ public class Parser implements IParser {
     private class AssignmentNode implements INode {
 
         private ExpressionNode eN = null;
-
+        private Lexeme lex;
 
         public AssignmentNode(Tokenizer tok) throws TokenizerException, IOException {
             System.out.println("AssignmentNode ");
             if (tok.current().token() == Token.IDENT) {
+                lex = tok.current();
                 System.out.println(tok.current() + " ASSIGN");
                 tok.moveNext();
                 if (tok.current().token() == Token.ASSIGN_OP) {
@@ -171,14 +181,19 @@ public class Parser implements IParser {
 
         @Override
         public void buildString(StringBuilder builder, int tabs) {
-
+            builder.append("AssignmentNode"+ "\r\n");
+            tabs++;
+            builder.append(lex + "\r\n");
+            builder.append(Token.ASSIGN_OP + "\r\n");
+            eN.buildString(builder, tabs);
+            builder.append(Token.SEMICOLON + "\r\n");
         }
     }
 
     private class ExpressionNode implements INode {
-
+        private ExpressionNode eN = null;
         private TermNode tM = null;
-
+        private Lexeme lex;
         public ExpressionNode(Tokenizer tok) throws TokenizerException, IOException {
 
             if (tok.current().token() == Token.IDENT || tok.current().token() == Token.INT_LIT
@@ -188,11 +203,14 @@ public class Parser implements IParser {
                 System.out.println("error here");
                 if (tok.current().token() == Token.ADD_OP || tok.current().token() == Token.SUB_OP) {
                     //terminal nodes
+                    lex = tok.current();
                     System.out.println(tok.current() + " EXPR if sub/add");
                     tok.moveNext();
-                    ExpressionNode eN = new ExpressionNode(tok);
+                    eN = new ExpressionNode(tok);
                     System.out.println("backinside expr");
                 }
+            }else {
+                throw new TokenizerException(TOKENIZERMESSAGE);
             }
 
         }
@@ -204,14 +222,21 @@ public class Parser implements IParser {
 
         @Override
         public void buildString(StringBuilder builder, int tabs) {
-
+            builder.append("ExpressionNode" + "\r\n");
+            tabs++;
+            tM.buildString(builder, tabs);
+            if ( lex != null) {
+                builder.append(lex + "\r\n");
+                eN.buildString(builder, tabs);
+            }
         }
     }
 
     private class TermNode implements INode {
 
         private FactorNode fN = null;
-
+        private TermNode tM = null;
+        private Lexeme lex;
         public TermNode(Tokenizer tok) throws IOException, TokenizerException {
 
             if (tok.current().token() == Token.IDENT || tok.current().token() == Token.INT_LIT
@@ -222,11 +247,15 @@ public class Parser implements IParser {
 
                 if (tok.current().token() == Token.MULT_OP || tok.current().token() == Token.DIV_OP) {
                     //terminal node
+                    lex = tok.current();
                     System.out.println(tok.current() + " Term");
                     tok.moveNext();
-                    TermNode tM = new TermNode(tok);
+                    tM = new TermNode(tok);
                 }
                 System.out.println("Closing if mul/div");
+            }
+            else {
+                throw new TokenizerException(TOKENIZERMESSAGE);
             }
             System.out.println("closing term");
         }
@@ -238,20 +267,26 @@ public class Parser implements IParser {
 
         @Override
         public void buildString(StringBuilder builder, int tabs) {
-
+            builder.append("TermNode" + "\r\n");
+            tabs++;
+            fN.buildString(builder, tabs);
+            builder.append(lex + "\r\n");
+            if ( tM != null) {
+                tM.buildString(builder, tabs);
+            }
         }
     }
 
     private class FactorNode implements INode {
-
-
+        private Lexeme lex;
+        private ExpressionNode eN = null;
         public FactorNode(Tokenizer tok) throws IOException, TokenizerException {
 
 
             if (tok.current().token() == Token.IDENT || tok.current().token() == Token.INT_LIT
                     || tok.current().token() == Token.LEFT_PAREN) {
                 System.out.println(tok.current() + " Factor inside if a Factor");
-
+                lex = tok.current();
                 //terminal node
 
                 if (tok.current().token() == Token.LEFT_PAREN) {
@@ -259,7 +294,7 @@ public class Parser implements IParser {
                     System.out.println(tok.current());
                     tok.moveNext();
                     System.out.println(tok.current() + " inside left paren if");
-                    ExpressionNode eN = new ExpressionNode(tok);
+                    eN = new ExpressionNode(tok);
                     System.out.println(tok.current() + " inside left paren if");
 
                     if (tok.current().token() == Token.RIGHT_PAREN) {
@@ -271,6 +306,8 @@ public class Parser implements IParser {
                 }
                 tok.moveNext();
                 System.out.println(tok.current() + " Factor end of factor");
+            }else {
+                throw new TokenizerException(TOKENIZERMESSAGE);
             }
         }
 
@@ -281,6 +318,13 @@ public class Parser implements IParser {
 
         @Override
         public void buildString(StringBuilder builder, int tabs) {
+            builder.append("FactorNode" + "\r\n");
+            tabs++;
+            builder.append(lex + "\r\n");
+            if(lex.token() == Token.LEFT_PAREN){
+                eN.buildString(builder, tabs);
+                builder.append(Token.RIGHT_PAREN);
+            }
 
         }
     }
